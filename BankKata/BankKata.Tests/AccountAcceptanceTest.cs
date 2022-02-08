d031671dcefff430d6f9bd0173d75b8fd89d801f
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-02-08 10:13:08 +0100

    red

---
+++ BankKata/BankKata.Tests/AccountAcceptanceTest.cs
@@ -0,0 +1,41 @@
+using System;
+using System.IO;
+using Xunit;
+
+namespace BankKata.Tests;
+
+/*
+Given a client makes a deposit of 1000 on 10-01-2012
+And a deposit of 2000 on 13-01-2012
+And a withdrawal of 500 on 14-01-2012
+When they print their bank statement
+Then they would see:
+
+Date       || Amount || Balance
+14/01/2012 || -500   || 2500
+13/01/2012 || 2000   || 3000
+10/01/2012 || 1000   || 1000
+ */
+
+public class AccountAcceptanceTest
+{
+    private const string Output = @"
+Date       || Amount || Balance
+14/01/2012 || -500   || 2500
+13/01/2012 || 2000   || 3000
+10/01/2012 || 1000   || 1000
+";
+
+    [Fact(DisplayName = "Account should follow a standard deposit and withdrawall process")]
+    public void Test1()
+    {
+        using var stringWriter = new StringWriter();
+        Console.SetOut(stringWriter);
+        var account = new Account();
+        account.Deposit(1000);
+        account.Deposit(2000);
+        account.Withdraw(500);
+        account.PrintStatement();
+        Assert.Equal(Output, stringWriter.ToString());
+    }
+}
--- BankKata/BankKata.Tests/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-using Xunit;
-
-namespace BankKata.Tests;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1() { }
-}
